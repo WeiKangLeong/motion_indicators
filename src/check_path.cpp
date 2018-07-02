0648a37eece412446cd8ac049fadb674a909f0f2
WeiKangLeong <leongweikang@hotmail.com>
2018-07-02 14:04:34 +0800

    adding check_path

--- src/check_path.cpp
+++ src/check_path.cpp
@@ -10,9 +10,37 @@ ros::Publisher pub;
 
 nav_msgs::Path stored_route;
 
+std::vector<std::vector<int>* >* path_;
+
 void store_path (const nav_msgs::Path path_in)
 {
     stored_route = path_in;
+
+    int path_fraction = stored_route.poses.size();
+
+    double path_gradient;
+
+    for (int i=1; i<path_fraction-1; i++)
+    {
+        double node_x0 = stored_route.poses[i-1].pose.position.x;
+        double node_y0 = stored_route.poses[i-1].pose.position.y;
+        double node_x1 = stored_route.poses[i].pose.position.x;
+        double node_y1 = stored_route.poses[i].pose.position.y;
+        double node_x2 = stored_route.poses[i+1].pose.position.x;
+        double node_y2 = stored_route.poses[i+1].pose.position.y;
+        double del_x1 = sqrt((node_x1-node_x0)*(node_x1-node_x0));
+        double del_y1 = sqrt((node_y1-node_y0)*(node_y1-node_y0));
+        double del_x2 = sqrt((node_x2-node_x1)*(node_x2-node_x1));
+        double del_y2 = sqrt((node_y2-node_y1)*(node_y2-node_y1));
+        double grad_1 = atan(del_y1/del_x1)*180/PI;
+        double grad_2 = atan(del_y2/del_x2)*180/PI;
+        std::cout<<"grad 1: "<<grad_1<<" "<<"grad 2: "<<grad_2<<std::endl;
+        if (std::abs(grad_1-grad_2)<5.0)
+        {
+
+        }
+
+    }
 }
 
 void 
@@ -41,6 +69,8 @@ main (int argc, char** argv)
 
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
+  path_ = new std::vector<std::vector<int>* >;
 
   // Spin
   ros::spin ();
